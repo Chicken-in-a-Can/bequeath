8e669c08de736b933f390bb68c58895c0a8f3a99
Ben Stidham <ben.stidham1@gmail.com>
2023-11-16 15:44:48 -0600

    I'm not gonna lie to you chief, i forgor

--- beqlang/src/build/build.rs
+++ beqlang/src/build/build.rs
@@ -1,8 +1,24 @@
-use std::{env::set_current_dir, collections::HashSet};
+use std::{
+    env::set_current_dir,
+    collections::HashSet,
+    process::{
+        Output,
+        Command
+    }
+};
 
 use crate::build::build_dir;
 
-#[derive(Eq, PartialEq, Hash)]
+pub const DEFAULT_DIR_CREATION: BuildType = BuildType::Fresh;
+pub const ALTERNATE_DIR_CREATION: BuildType = BuildType::Update;
+
+pub const DEFAULT_COMPILE_TYPE: BuildType = BuildType::Debug;
+pub const ALTERNATE_COMPILE_TYPE: BuildType = BuildType::Release;
+
+pub const DEFAULT_RUN_TYPE: BuildType = BuildType::RunRelease;
+pub const ALTERNATE_RUN_TYPE: BuildType = BuildType::RunDebug;
+
+#[derive(Eq, PartialEq, Hash, Copy, Clone, Debug)]
 pub enum BuildType {
     Fresh,
     Update,
@@ -12,11 +28,41 @@ pub enum BuildType {
     RunRelease,
 }
 
-pub struct Build{}
+pub struct Build{
+    build_options: HashSet<BuildType>,
+    build_dir: String,
+}
 
 impl Build{
-    pub fn build(build_options: HashSet<BuildType>, build_dir: String){
-        let build_dir_name = build_dir::gen_build_dir(build_options, build_dir);
-        let _result = set_current_dir(build_dir_name);
+    pub fn new(build_options: HashSet<BuildType>, build_dir: String) -> Self{
+        return Self{
+            build_options,
+            build_dir,
+        }
+    }
+    pub fn build(&self){
+        let build_dir_name = build_dir::gen_build_dir(self.build_options.clone(), self.build_dir.clone());
+        match set_current_dir(build_dir_name){
+            Ok(_result) => (),
+            Err(_e) => panic!("Error\nCould not change directory"),
+        }
+        let build_command: String;
+        if self.build_options.contains(&BuildType::Debug) || (!self.build_options.contains(&BuildType::Release) && DEFAULT_COMPILE_TYPE == BuildType::Debug){
+            build_command = String::from("cargo build");
+        } else{
+            build_command = String::from("cargo build --release");
+        }
+        let rust_init: Output = if cfg!(target_os = "windows"){
+            Command::new("cmd")
+                .args(["/C", &build_command])
+                .output()
+                .expect("Failed to build cargo project")
+        } else{
+            Command::new("sh")
+                .args(["-c", &build_command])
+                .output()
+                .expect("Failed to build cargo project")
+        };
+        let _init_output = rust_init.stdout;
     }
 }
--- beqlang/src/build/build_dir.rs
+++ beqlang/src/build/build_dir.rs
@@ -1,35 +1,26 @@
 use std::collections::HashSet;
-use crate::build::build::BuildType;
+use crate::build::build::{self, BuildType};
 
 use std::fs;
 use std::process::{Output, Command};
 
-const DEFAULT_DIR_CREATION: BuildType = BuildType::Fresh;
-const ALTERNATE_DIR_CREATION: BuildType = BuildType::Update;
-
-const DEFAULT_COMPILE_TYPE: BuildType = BuildType::Debug;
-const ALTERNATE_COMPILE_TYPE: BuildType = BuildType::Release;
-
-const DEFAULT_RUN_TYPE: BuildType = BuildType::RunRelease;
-const ALTERNATE_RUN_TYPE: BuildType = BuildType::RunDebug;
-
 pub fn gen_build_dir(build_options: HashSet<BuildType>, dir_name: String) -> String{
-    let dir_creation: BuildType = if build_options.contains(&DEFAULT_DIR_CREATION){
-        DEFAULT_DIR_CREATION
+    let dir_creation: BuildType = if build_options.contains(&build::DEFAULT_DIR_CREATION){
+        build::DEFAULT_DIR_CREATION
     } else{
-        ALTERNATE_DIR_CREATION
+        build::ALTERNATE_DIR_CREATION
     };
 
-    let _compile_type: BuildType = if build_options.contains(&DEFAULT_COMPILE_TYPE){
-        DEFAULT_COMPILE_TYPE
+    let _compile_type: BuildType = if build_options.contains(&build::DEFAULT_COMPILE_TYPE){
+        build::DEFAULT_COMPILE_TYPE
     } else {
-        ALTERNATE_COMPILE_TYPE
+        build::ALTERNATE_COMPILE_TYPE
     };
 
-    let run_type: BuildType = if build_options.contains(&DEFAULT_RUN_TYPE){
-        DEFAULT_RUN_TYPE
+    let run_type: BuildType = if build_options.contains(&build::DEFAULT_RUN_TYPE){
+        build::DEFAULT_RUN_TYPE
     } else {
-        ALTERNATE_RUN_TYPE
+        build::ALTERNATE_RUN_TYPE
     };
 
 
@@ -52,7 +43,6 @@ pub fn gen_build_dir(build_options: HashSet<BuildType>, dir_name: String) -> Str
                 .output()
                 .expect("Failed to initialize cargo in directory")
         };
-
         let _init_output = rust_init.stdout;
     }
 
